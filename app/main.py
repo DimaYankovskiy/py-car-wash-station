fdf0d2b79205e88c4c6e0bf17e72c36c880a054b
Dima <dima2000yan@gmail.com>
2024-05-21 10:36:20 +0300

    car-wash-station solution fix

--- app/main.py
+++ app/main.py
@@ -19,7 +19,7 @@ class CarWashStation:
         self.average_rating = average_rating
         self.count_of_ratings = count_of_ratings
 
-    def serve_cars(self, cars: list[Car]) -> float:
+    def serve_cars(self, cars: list[Car]) -> float | int:
         income = 0
         for car in cars:
             if car.clean_mark < self.clean_power:
@@ -28,19 +28,28 @@ class CarWashStation:
         return income
 
     def calculate_washing_price(self, car: Car) -> float:
-        return round(car.comfort_class
-                     * (self.clean_power
-                        - car.clean_mark)
-                     * (self.average_rating
-                        / self.distance_from_city_center), 1)
+        comfort_class = car.comfort_class
+        clean_power_difference = self.clean_power - car.clean_mark
+        average_rating = self.average_rating
+        distance_from_city_center = self.distance_from_city_center
+
+        result = (comfort_class
+                  * clean_power_difference
+                  * (average_rating / distance_from_city_center))
+
+        final_result = round(result, 1)
+
+        return final_result
 
     def wash_single_car(self, car: Car) -> None:
         if self.clean_power > car.clean_mark:
             car.clean_mark = self.clean_power
 
     def rate_service(self, new_rating: float) -> None:
-        self.average_rating = round(
-            (self.average_rating
-             * self.count_of_ratings + new_rating)
-            / (self.count_of_ratings + 1), 1)
+        total_rating_sum = self.average_rating * self.count_of_ratings
+        new_total_rating_sum = total_rating_sum + new_rating
+        new_count_of_ratings = self.count_of_ratings + 1
+        new_average_rating = new_total_rating_sum / new_count_of_ratings
+
+        self.average_rating = round(new_average_rating, 1)
         self.count_of_ratings += 1
